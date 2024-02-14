95aa2ec5d1bd64f18acb9db4b7d4fdddd8d30fc3
Jin <jinyung7428@gmail.com>
2024-02-14 10:43:28 -0500

    Lab assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Labs from './Labs';
-import Kanbas from './Kanbas';
 import HelloWorld from './Labs/a3/HelloWorld';
+import Kanbas from './Kanbas';
 import { HashRouter } from 'react-router-dom';
 import { Routes, Route, Navigate } from 'react-router';
-
 function App() {
     return (
         <HashRouter>
@@ -14,12 +10,11 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/Labs" />} />
                     <Route path="/Labs/*" element={<Labs />} />
-                    <Route path="/kanbas/*" element={<Kanbas />} />
+                    <Route path="/Kanbas/*" element={<Kanbas />} />
                     <Route path="/hello" element={<HelloWorld />} />
                 </Routes>
             </div>
         </HashRouter>
     );
 }
-
 export default App;
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,6 +1,8 @@
+import Nav from '../Nav';
 function Kanbas() {
     return (
         <div>
+            <Nav />
             <h1>Kanbas</h1>
         </div>
     );
